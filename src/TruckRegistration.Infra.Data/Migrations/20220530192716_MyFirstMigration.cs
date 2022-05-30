09949dcaa19422985ff2c97900d5ce7e7ec72702
Rafael Rodrigues <rafael.rodrigues.dsv@outlook.com>
2022-05-30 16:31:22 -0300

    AddFirstMigration

---
+++ src/TruckRegistration.Infra.Data/Migrations/20220530192716_MyFirstMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TruckRegistration.Infra.Data.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Truck",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    Description = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Model = table.Column<string>(type: "varchar(3)", maxLength: 3, nullable: false),
+                    ManufactureYear = table.Column<int>(type: "int", nullable: false),
+                    ModelYear = table.Column<int>(type: "int", maxLength: 3, nullable: false),
+                    Renavam = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false),
+                    Color = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Truck", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Truck");
+        }
+    }
+}
