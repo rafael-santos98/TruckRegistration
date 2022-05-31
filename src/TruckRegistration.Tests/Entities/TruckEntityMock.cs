c7bf77d55d692e5459138c42b112f3d61f1dcc4a
Rafael Rodrigues <rafael.rodrigues.dsv@outlook.com>
2022-05-31 01:08:32 -0300

    Adiciona GetAll_Test

---
+++ src/TruckRegistration.Tests/Entities/TruckEntityMock.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TruckRegistration.Application.Models.Response;
+using TruckRegistration.Domain.Entities;
+
+namespace TruckRegistration.Tests.Entities
+{
+    public static class TruckEntityMock
+    {
+        public static Truck GetTruck()
+        {
+            return GetTruckList().FirstOrDefault();
+        }
+
+        public static List<Truck> GetTruckList()
+        {
+            return new List<Truck>{
+                new Truck()
+                {
+                    Id = new System.Guid(),
+                    Description = "",
+                    Model = Domain.Entities.Enums.EModel.FM,
+                    ManufactureYear = 2022,
+                    ModelYear = 2023,
+                    Renavam = "30808965591",
+                    Color = "Color"
+                },
+                new Truck()
+                {
+                    Id = new System.Guid(),
+                    Description = "",
+                    Model = Domain.Entities.Enums.EModel.FM,
+                    ManufactureYear = 2022,
+                    ModelYear = 2023,
+                    Renavam = "22709827033",
+                    Color = "White"
+                }
+            };
+        }
+
+        public static TruckResponse GetTruckResponse()
+        {
+            return GetTruckResponseList().FirstOrDefault();
+        }
+
+        public static List<TruckResponse> GetTruckResponseList()
+        {
+            return new List<TruckResponse>{
+                new TruckResponse()
+                {
+                    Id = new System.Guid(),
+                    Description = "",
+                    Model = Domain.Entities.Enums.EModel.FM,
+                    ManufactureYear = 2022,
+                    ModelYear = 2023,
+                    Renavam = "30808965591",
+                    Color = "Color"
+                },
+                new TruckResponse()
+                {
+                    Id = new System.Guid(),
+                    Description = "",
+                    Model = Domain.Entities.Enums.EModel.FM,
+                    ManufactureYear = 2022,
+                    ModelYear = 2023,
+                    Renavam = "22709827033",
+                    Color = "White"
+                }
+            };
+        }
+    }
+}
--- src/TruckRegistration.Tests/UnitTests/Application/TruckServiceAppTest.cs
+++ src/TruckRegistration.Tests/UnitTests/Application/TruckServiceAppTest.cs
@@ -1,17 +1,52 @@
-﻿using Xunit;
+﻿using AutoMapper;
+using FluentAssertions;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TruckRegistration.Application.Models.Response;
+using TruckRegistration.Application.Services;
+using TruckRegistration.Domain.Contracts.Commands;
+using TruckRegistration.Domain.Contracts.Repositories;
+using TruckRegistration.Domain.Entities;
+using TruckRegistration.Tests.Entities;
+using Xunit;
 
 namespace TruckRegistration.Tests.UnitTests.Application
 {
     public class TruckServiceAppTest
     {
+
+        private readonly Mock<IMapper> _mapperMock;
+        private readonly Mock<ITruckCommandHandler> _truckCommandHandlerMock;
+        private readonly Mock<ITruckRepository> _truckRepositoryMock;
+        private readonly TruckAppService _truckAppService;
+
         public TruckServiceAppTest()
         {
+            _mapperMock = new Mock<IMapper>();
+            _truckCommandHandlerMock = new Mock<ITruckCommandHandler>();
+            _truckRepositoryMock = new Mock<ITruckRepository>();
+
+            _truckAppService = new TruckAppService(_mapperMock.Object,
+                _truckCommandHandlerMock.Object,
+                _truckRepositoryMock.Object);
         }
 
+        //[Fact(DisplayName = "Get All Trucks with items")]
         [Fact]
         public void GetAll_Test()
         {
+            // Arrange
+            _mapperMock.Setup(m => m.Map<IEnumerable<TruckResponse>>(It.IsAny<List<Truck>>()))
+                .Returns(TruckEntityMock.GetTruckResponseList);
 
+            _truckRepositoryMock.Setup(m => m.GetAll(false)).ReturnsAsync(TruckEntityMock.GetTruckList);
+
+            // Act
+            var result = _truckAppService.GetAll().GetAwaiter().GetResult();
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
         }
     }
 }
