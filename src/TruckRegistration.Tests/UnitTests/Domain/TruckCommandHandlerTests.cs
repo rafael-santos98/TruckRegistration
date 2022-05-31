7a5a2d93393eebe615ae9ff388281c84428ef8a5
Rafael Rodrigues <rafael.rodrigues.dsv@outlook.com>
2022-05-31 02:36:21 -0300

    Ajustes testes de Handler

--- src/TruckRegistration.Tests/UnitTests/Domain/TruckCommandHandlerTests.cs
+++ src/TruckRegistration.Tests/UnitTests/Domain/TruckCommandHandlerTests.cs
@@ -85,5 +85,39 @@ namespace TruckRegistration.Tests.UnitTests.Domain
             result.ValidationResult.IsValid.Should().BeFalse();
             result.ValidationResult.Errors.Count.Should().BeGreaterThan(0);
         }
+
+        [Fact(DisplayName = "Delete Truck with success")]
+        public async Task Delete_Truck_Test_With_Success()
+        {
+            // Arrange
+            _truckRepositoryMock.Setup(m => m.GetById(It.IsAny<System.Guid>(), It.IsAny<bool>()))
+                .ReturnsAsync(TruckEntityMock.GetTruck());
+
+            _truckRepositoryMock.Setup(m => m.Remove(It.IsAny<System.Guid>()))
+                .ReturnsAsync(true);
+
+            _truckRepositoryMock.Setup(m => m.Commit());
+
+            // Act
+            var result = await _truckCommandHandler.Delete(TruckEntityMock.GetTruck().Id);
+
+            // Assert
+            result.ValidationResult.IsValid.Should().BeTrue();
+        }
+
+        [Fact(DisplayName = "Delete Truck with errors")]
+        public async Task Delete_Truck_Test_With_Errors()
+        {
+            // Arrange
+            _truckRepositoryMock.Setup(m => m.GetById(It.IsAny<System.Guid>(), It.IsAny<bool>()))
+                .ReturnsAsync(It.IsAny<Truck>());
+
+            // Act
+            var result = await _truckCommandHandler.Delete(It.IsAny<System.Guid>());
+
+            // Assert
+            result.ValidationResult.IsValid.Should().BeFalse();
+            result.ValidationResult.Errors.Count.Should().BeGreaterThan(0);
+        }
     }
 }
