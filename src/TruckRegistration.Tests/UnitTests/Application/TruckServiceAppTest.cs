65776e27fe28f8d3e61f8a5126c9300290681bb7
Rafael Rodrigues <rafael.rodrigues.dsv@outlook.com>
2022-05-30 23:11:59 -0300

    Adiciona projeto de testes

---
+++ src/TruckRegistration.Tests/UnitTests/Application/TruckServiceAppTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace TruckRegistration.Tests.UnitTests.Application
+{
+    public class TruckServiceAppTest
+    {
+        public TruckServiceAppTest()
+        {
+        }
+
+        [Fact]
+        public void GetAll_Test()
+        {
+
+        }
+    }
+}
