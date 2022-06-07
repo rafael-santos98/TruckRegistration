2a8a71644dc11980fb50f90b98e674044488f401
Rafael Rodrigues <rafael.rodrigues.dsv@gmail.com>
2022-06-07 14:25:06 -0300

    Deleta classe não utilizada

--- src/TruckRegistration.Tests.IntegrationTests/Controllers/TruckControllerTest.cs
+++ src/TruckRegistration.Tests.IntegrationTests/Controllers/TruckControllerTest.cs
@@ -1,6 +1,28 @@
-﻿namespace TruckRegistration.Tests.ComponentTests.Controllers
+﻿using System.Threading.Tasks;
+using TruckRegistration.Services.Api;
+using TruckRegistration.Tests.ComponentTests.Config;
+using Xunit;
+
+namespace TruckRegistration.Tests.ComponentTests.Controllers
 {
-    internal class TruckControllerTest
+    [Collection(nameof(IntegrationApiTestsFixtureCollection))]
+    public class TruckControllerTest
     {
+        private readonly IntegrationTestsFixture<StartupTest> _testsFixture;
+
+        public TruckControllerTest(IntegrationTestsFixture<StartupTest> testsFixture)
+        {
+            _testsFixture = testsFixture;
+        }
+
+        [Fact(DisplayName = "Get All Trucks contains items")]
+        public async Task GetAll_Test_Contains_Items()
+        {
+            // Arrange & Act
+            var response = await _testsFixture.Client.GetAsync("truck-registration/v1/trucks");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
     }
 }
